d42dc9f1be5b2c7caa6c26d56f9d3cb9bfd4429f
Chris Matthieu <chris@matthieu.us>
2011-01-07 18:51:45 -0700

    reworking npmfetch

--- npmfetch.js
+++ npmfetch.js
@@ -6,6 +6,7 @@ var request = require('request'),
 	spawn = require('child_process').spawn;
 	
 var exec  = require('child_process').exec;
+var open_files_count = 0
 
 request({uri:'http://registry.npmjs.org'}, function (error, response, body) {
   if (!error && response.statusCode == 200) {
@@ -20,3 +21,29 @@ request({uri:'http://registry.npmjs.org'}, function (error, response, body) {
 	};
   };
 });
+
+
+function handleError(err) {
+	sys.puts('Error!!! encountered ' + err);
+	process.exit();
+}
+
+function readFiles(files) {
+	files.forEach(function(file) {
+		var filename = cwd + '/_posts/' + file.toString();
+		readFile(filename);
+	});
+}
+
+function readFile(file) {
+	if (open_files_count > 200) {
+		global.setTimeout(readFile, 1000, file);
+	} else {
+		open_files_count++;
+		fs.readFile(file, function(err, data) {
+			if (err) handleError(err);
+			open_files_count--;
+      sys.debug(data);
+		});
+	}
+}
