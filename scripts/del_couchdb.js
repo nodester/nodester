5fe7b54d16a35688ce1a9dce1cf8da2c6f1b671e
Daniel Bartlett <dan@f-box.org>
2011-01-21 09:47:09 +0000

    Adding couchdb setup/del scripts -- don't handle auth yet :/

---
+++ scripts/del_couchdb.js
@@ -0,0 +1,32 @@
+
+var sys = require('sys');
+var http = require('http');
+var config = require('./config');
+
+var delete_couchdb_table = function (port, hostname, tablename, callback) {
+  var co = http.createClient(port, hostname);
+  var req = co.request('PUT', '/' + tablename, {host: hostname});
+  var rtv = false;
+  req.end();
+  req.on('response', function (resp) {
+    switch (resp.statusCode) {
+      case 200:
+        rtv = true;
+        break;
+    }
+    callback(rtv);
+  });
+};
+
+for(var i in config.opt.couch_tables) {
+  (function () {
+  var tabname = config.opt.couch_prefix + "_" + config.opt.couch_tables[i];
+  create_couchdb_table(config.opt.couch_port, config.opt.couch_host, tabname, function (success) {
+    if (success) {
+      sys.puts(tabname + " was deleted.");
+    } else {
+      sys.puts(tabname + " failed to be deleted.");
+    }
+  });
+  })();
+}
