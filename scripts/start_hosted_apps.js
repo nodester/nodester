cf455578414ff0f184a2d7afb8bc570f617b2652
Daniel Bartlett <dan@f-box.org>
2011-01-24 16:21:55 +0000

    fixing start apps so it actually works

--- scripts/start_hosted_apps.js
+++ scripts/start_hosted_apps.js
@@ -5,9 +5,15 @@ var sys = require('sys');
 var exec = require('child_process').exec;
 
 var couch_http = http.createClient(config.opt.couch_port, config.opt.couch_host);
+if (config.opt.couch_prefix.length > 0) {
+  var cprefix = config.opt.couch_prefix + '_';
+} else {
+  var cprefix = '';
+}
+
 var request = couch_http.request(
   'GET',
-  '/apps' + '/_design/nodeapps/_view/all',
+  '/' + cprefix + 'apps' + '/_design/nodeapps/_view/all',
   {
     'host': config.opt.couch_host,
     'Authorization': "Basic " + base64_encode(new Buffer(config.opt.couch_user + ":" + (config.opt.couch_pass || "")))
@@ -17,6 +23,7 @@ request.end();
 request.on('response', function (response) {
   var buff = '';
   if (response.statusCode != 200) {
+    console.log(response.statusCode);
     console.log('Error: Cannot query CouchDB');
     process.exit(1);
   }
@@ -35,8 +42,7 @@ var start_running_apps = function (apps_arr) {
     var doc = apps_arr[i].value;
     if (doc.running == 'true') {
       var app_user_home = config.opt.home_dir + '/' + config.opt.hosted_apps_subdir + '/' + doc.username;
-      var app_home = app_user_home + '/' + doc.repo_id;
-      var cmd = "sudo " + config.opt.app_dir + '/scripts/launch_app.sh ' + config.opt.app_dir + ' ' + app_user_home + ' ' + app_home + ' ' + doc.start;
+      var cmd = "sudo " + config.opt.app_dir + '/scripts/launch_app.sh ' + config.opt.app_dir + ' ' + app_user_home + ' ' + doc.repo_id + ' ' + doc.start;
       var child = exec(cmd, function (error, stdout, stderr) {});
     }
   }
