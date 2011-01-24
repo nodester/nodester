a2574c31fa4d12328cf63f89b34328efbcc754bc
Daniel Bartlett <dan@f-box.org>
2011-01-24 16:22:24 +0000

    Adding small test script

---
+++ scripts/test_couchdb.js
@@ -0,0 +1,20 @@
+
+var sys = require('sys');
+var http = require('http');
+var config = require('../config');
+var base64_encode = require('base64').encode;
+var request = require('request');
+
+var h = {accept: 'application/json', 'content-type': 'application/json'};
+var couch_loc = "http://" + config.opt.couch_user + ":" + config.opt.couch_pass + "@" + config.opt.couch_host + ":" + config.opt.couch_port + "/";
+if (config.opt.couch_prefix.length > 0) {
+  couch_loc += config.opt.couch_prefix + "_";
+}
+
+request({uri:couch_loc + 'nextport/port', method:'GET', headers:h}, function (err, response, body) {
+  sys.puts(sys.inspect(err));
+  sys.puts(sys.inspect(response));
+  sys.puts(sys.inspect(body));
+  var doc = JSON.parse(body);
+  sys.puts(sys.inspect(doc));
+});
