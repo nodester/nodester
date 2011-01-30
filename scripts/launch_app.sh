a36c1c09fdae156e07e83eb82baa1d48443aa6c8
Chris Matthieu <chris@matthieu.us>
2011-01-30 12:39:35 -0700

    updated node path in launch_app

--- scripts/launch_app.sh
+++ scripts/launch_app.sh
@@ -5,7 +5,7 @@ APPDIR=${3};
 START=${4};
 
 if [ -f ${BASEDIR}/scripts/launch_chrooted_app.js ]; then
-  exec /usr/bin/node ${BASEDIR}/scripts/launch_chrooted_app.js ${USERDIR}/${APPDIR} ${START};
+  exec /usr/local/bin/node ${BASEDIR}/scripts/launch_chrooted_app.js ${USERDIR}/${APPDIR} ${START};
 else
   exec /usr/local/bin/node ${BASEDIR}/deps/nodemon/nodemon ${USERDIR} ${APPDIR}/.app.pid ${APPDIR}/${START};
 fi;
