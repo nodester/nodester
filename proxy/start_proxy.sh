7a57607621b4ed35ec5438afd6676be7e726994f
Daniel Bartlett <dan@f-box.org>
2011-02-11 10:54:34 +0000

    Moving back to stock nodemon due to launch_chroot_app.js being used now.

--- proxy/start_proxy.sh
+++ proxy/start_proxy.sh
@@ -4,4 +4,4 @@ APPDIR=${2};
 export PATH="/usr/local/bin:${PATH}";
 ulimit -n 99999;
 cd ${APPDIR}/proxy;
-exec ${APPDIR}/deps/nodemon/nodemon /var/run/nodester_proxy.pid ${APPDIR}/proxy/proxy.js
+exec ${HOME}/bin/nodemon ${APPDIR}/proxy/proxy.js
--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 APPPATH=`pwd`;
 
 sudo ${APPPATH}/proxy/start_proxy.sh ${HOME} ${APPPATH}
-${APPPATH}/deps/nodemon/nodemon ${APPPATH}/.app.pid ${APPPATH}/app.js
+nodemon ${APPPATH}/app.js
 node ${APPPATH}/scripts/start_hosted_apps.js
