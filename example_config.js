16eddb266967ff9d54fcfcbd8bc9b49653bdaf19
Daniel Bartlett <dan@f-box.org>
2011-02-11 11:03:19 +0000

    Adding SSL support to proxy.js, enabling the use of proxytable for host matching. Dependencies need changing, hold tight...

--- example_config.js
+++ example_config.js
@@ -3,17 +3,22 @@ exports.opt = {
   couch_pass: 'password',
   couch_host: "127.0.0.1",
   couch_port: 5984,
-  couch_prefix: 'test',
+  couch_prefix: 'nodester',
   couch_tables: ['coupons', 'nodefu', 'nextport', 'apps', 'repos', 'aliasdomains'],
   home_dir: '/var/nodester',
   app_dir: '/var/nodester/nodester',
-  hosted_apps_subdir: 'hosted_apps', // This should be a subfolder of home_dir
+  hosted_apps_subdir: 'hosted_apps', // This should be a subfolder of home_dir - TODO - Change this..
+  proxy_table_file: '/var/nodester/var/proxy_table.json',
   tl_dom: 'testnodester.com',
   api_dom: 'api.testnodester.com',
   git_user: 'nodester',
   git_dom: 'testnodester.com',
-  coupon_code: 'PlzCanIHazNodeJS',
-  blocked_apps: ['www', 'api', 'support', 'dan'],
+  coupon_code: 'CouponCode',
+  blocked_apps: ['www', 'api'],
   restart_key: 'KeepThisSecret',
-  userid: "nodester"
+  userid: "nodester",
+  enable_ssl: false, // Currently SSL forward to the app/api, when I have a wildcard cert to test, then all apps can have SSL.
+  ssl_ca_file: "",
+  ssl_cert_file: "",
+  ssl_key_file: ""
 };
--- proxy/proxy.js
+++ proxy/proxy.js
@@ -1,3 +1,4 @@
+#!/usr/bin/node
 /*
 
 Nodester - Nodejs hosting
@@ -6,115 +7,46 @@ This app runs on port 80 and forwards traffic to the appropriate node app
 */
 
 var httpProxy = require('http-proxy'),
+    https = require('https'),
     url = require('url'),
     sys = require('sys'),
-    daemontools = require('daemon-tools');
+    fs = require('fs'),
+    lib = require('../lib'),
+    daemontools = require('daemon-tools')
+    config = require('../config')
+;
 
-var request = require('request');
-var h = {accept:'application/json', 'content-type':'application/json'};
-
-var config = require("../config");
-var couch_loc = "http://" + config.opt.couch_user + ":" + config.opt.couch_pass + "@" + config.opt.couch_host + ":" + config.opt.couch_port + "/";
-if (config.opt.couch_prefix.length > 0) {
-  couch_loc += config.opt.couch_prefix + "_";
-}
-
-var handle_incoming = function (req, res, proxy, websocket) {
-  if (typeof req.headers.host != 'undefined') {
-    var hostname = req.headers.host.toLowerCase();;
-    var first_point = hostname.indexOf(".");
-    var subdomain = hostname.substring(0, first_point);
-    var restdom = hostname.substring(first_point + 1);
-    if (hostname.indexOf("nodefu") != -1 && websocket == false) {
-      res.writeHead(301, {'Content-Type': 'text/plain', 'Location': 'http://nodester.com'});
-      res.end();
-    } else if (hostname == config.opt.api_dom) {
-      // API Domain
-      proxy.proxyRequest(4001, '127.0.0.1');
-    } else if (hostname != config.opt.tl_dom && (restdom == config.opt.tl_dom && subdomain != '' && subdomain != 'www' && hostname != '127.0.0.1')) {
-      request({uri:couch_loc + 'apps/' + subdomain, method:'GET', headers:h}, function (err, response, body) {
-        var doc = JSON.parse(body);
-        if (doc) {
-          if (doc.running == 'true') {
-            if (typeof websocket != 'undefined' && websocket == true) {
-              proxy.proxyWebSocketRequest(doc.port, '127.0.0.1', req.headers.host);
-            } else {
-              proxy.proxyRequest(doc.port, '127.0.0.1');
-            }
-          } else {
-            if (websocket == false) {
-              res.writeHead(503, {'Content-Type': 'text/plain'});
-              res.end('app is not running');
-            } else {
-              // Not sure here!
-            }
-          }
-        } else {
-          if (websocket == false) {
-            res.writeHead(503, {'Content-Type': 'text/plain'});
-            res.end('app does not exist');
-          } else {
-            // Not sure here!
-          }
-        }
-      });
-    } else {
-      // Look up hosted domains
-      request({uri:couch_loc + 'aliasdomains/' + hostname, method:'GET', headers:h}, function (err, response, body) {
-        var doc = JSON.parse(body);
-        if (doc) {
-          if (typeof doc.error != 'undefined' && doc.error == "not_found") {
-            proxy.proxyRequest(4001, '127.0.0.1');
-          } else if (typeof doc.appname != 'undefined') {
-            request({uri:couch_loc + 'apps/' + doc.appname, method:'GET', headers:h}, function (err, response, body) {
-              var doc = JSON.parse(body);	
-              if (doc) {
-                if (doc.running == 'true') {
-                  if (typeof websocket != 'undefined' && websocket == true) {
-                    proxy.proxyWebSocketRequest(doc.port, '127.0.0.1', req.headers.host);
-                  } else {
-                    proxy.proxyRequest(doc.port, '127.0.0.1');
-                  }
-                } else {
-                  if (websocket == false) {
-                    res.writeHead(503, {'Content-Type': 'text/plain'});
-                    res.end('app is not running');
-                  } else {
-                    // Not sure here!
-                  }
-                }
-              } else {
-                if (websocket == false) {
-                  res.writeHead(503, {'Content-Type': 'text/plain'});
-                  res.end('app does not exist');
-                } else {
-                  // Not sure here!
-                }
-              }
-            });
-          } else {
-            proxy.proxyRequest(4001, '127.0.0.1');
-          }
-        }
-      });
-    }
+lib.update_proxytable_map(function (err) {
+  if (err) {
+    console.log("err: " + JSON.stringify(err));
   } else {
-    proxy.proxyRequest(4001, '127.0.0.1');
+    // var proxy = httpProxy.createServer({router: config.opt.proxy_table_file});
+    var proxy = httpProxy.createServer({router: config.opt.proxy_table_file, silent: false, hostname_only: true});
+    proxy.listen(80);
+    proxy.addListener('updateRoutes', function () {
+      console.log('updateRoutes fired');
+    });
+    if (config.opt.enable_ssl === true) {
+      var options = {
+        ca: fs.readFileSync(config.opt.app_dir + '/' + config.opt.ssl_ca_file),
+        key: fs.readFileSync(config.opt.app_dir + '/' + config.opt.ssl_key_file),
+        cert: fs.readFileSync(config.opt.app_dir + '/' + config.opt.ssl_cert_file)
+      };
+      var httpSsl = https.createServer(options, function (req, res) {
+        var proxy = new httpProxy.HttpProxy(req, res);
+        proxy.proxyRequest(4001, "127.0.0.1", req, res);
+      });
+      httpSsl.setMaxListeners(1000);
+      httpSsl.listen(443);
+      sys.puts('Nodester started on port 443');
+    }
+    daemontools.setreuid_username(config.opt.userid);
+    sys.puts('Nodester started on port 80');
   }
-};
-
-var prox = httpProxy.createServer(function (req, res, proxy) {
-  handle_incoming(req, res, proxy, false);
 });
-prox.addListener('upgrade', function (req, sock, head) {
-  var proxy = new httpProxy.HttpProxy(req, sock, head);
-  handle_incoming(req, undefined, proxy, true);
-});
-prox.listen(80);
-sys.puts('Nodester started on port 80');
 
-daemontools.setreuid_username(config.opt.username);
-
+/*
 process.on('uncaughtException', function (err) {
   console.log(err);
 });
+*/
