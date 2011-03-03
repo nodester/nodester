f888d4c67e81e20fe45c19908f2d2d002c3ea12f
Dav Glass <davglass@gmail.com>
2011-03-03 14:25:13 -0600

    Updated gitrepoclone.sh file

--- scripts/example_gitrepoclone.sh
+++ scripts/example_gitrepoclone.sh
@@ -1,17 +1,34 @@
-#! /bin/bash
+#!/bin/bash
 # post-commit hook to create git file directory for node subdomain 
 SECRETKEY=PleaseRestartMyAppMKay
-# cd ..
+OLD_PWD=$PWD
 gitdirsuffix=${PWD##*/}
 gitdir=${gitdirsuffix%.git}
 
 if [ -d "../$gitdir" ]; then
+  echo "Syncing repo with chroot"
   cd ../$gitdir;
   unset GIT_DIR;
   git pull;
 else
+  echo "Fresh git clone into chroot"
   git clone . ../$gitdir/;
   cd ../$gitdir;
-fi;
+fi
 
-curl "http://127.0.0.1:4001/app_restart?repo_id=${gitdir}&restart_key=${SECRETKEY}" >/dev/null 2>&1
+hook=./.git/hooks/post-receive
+echo "Checking $hook"
+if [ -d "$hook" ]; then
+    echo "Removing $hook"
+    rm $hook
+fi
+
+echo "cd $OLD_PWD"
+cd $OLD_PWD
+
+echo "Attempting to restart your app: ${gitdir}"
+curl "http://127.0.0.1:4001/app_restart?repo_id=${gitdir}&restart_key=${SECRETKEY}" >/dev/null 2>&1 &
+echo "App restarted.."
+echo ""
+exit 0;
+
