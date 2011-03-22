1332efad0d8068e88347f51e5388597a3ca83979
Dav Glass <davglass@gmail.com>
2011-03-22 09:38:18 -0500

    Minor script updates

--- bin/app_start.sh
+++ bin/app_start.sh
@@ -5,13 +5,12 @@ cd `dirname $0`
 ./node-config.js
 wait
 source ./.nodester.config
+rm ./.nodester.config
 
 export HOME=$HOME_DIR;
-export PATH="/usr/local/bin:${PATH}";
+export PATH="/usr/local/bin:${HOME}/bin:${PATH}";
 
 APPDIR=$APP_DIR
-
-
 FHOME=$HOME/forever-app/
 
 if [ ! -d $FHOME ]; then
--- bin/app_stop.sh
+++ bin/app_stop.sh
@@ -5,9 +5,10 @@ cd `dirname $0`
 ./node-config.js
 wait
 source ./.nodester.config
+rm ./.nodester.config
 
 export HOME=$HOME_DIR;
-export PATH="/usr/local/bin:${PATH}";
+export PATH="/usr/local/bin:${HOME}/bin:${PATH}";
 
 APPDIR=$APP_DIR
 
@@ -15,3 +16,5 @@ APPDIR=$APP_DIR
 FHOME=$HOME/forever-app
 
 forever stopall -p $FHOME
+
+sleep 1
