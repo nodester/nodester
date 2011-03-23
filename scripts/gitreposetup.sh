0562f9f4f2bfe1dc1178dcfcda2aa5bc5f96ef78
Dav Glass <davglass@gmail.com>
2011-03-22 22:20:01 -0500

    Fixing repo permissions

--- scripts/gitreposetup.sh
+++ scripts/gitreposetup.sh
@@ -7,6 +7,7 @@ APPDIR=${2};
 APPUSERNAME=${3};
 REV=${4};
 START=${5};
+UID=${6};
 
 if [ ! -d "${APPDIR}" ]; then
   mkdir -p ${APPDIR};
@@ -19,3 +20,8 @@ cp ${BASEDIR}/scripts/gitrepoclone.sh hooks/post-receive;
 chmod +x hooks/post-receive;
 
 git clone . ../${REV}/;
+wait
+cd ../
+chown -R $UID ${REV}
+wait
+chown -R $UID ${REV}.git
