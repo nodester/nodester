fe800d492862635cfeff10b6ed8ece4c8e4ec96a
DanBUK <dan@f-box.org>
2011-06-20 10:28:58 +0100

    Updating proxy to handle upgrade requests.

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -6,7 +6,9 @@ This app runs on port 80 and forwards traffic to the appropriate node app
 
 */
 
-var httpProxy = require('../lib/3rdparty/node-http-proxy'),
+var httpProxy = require('http-proxy'),
+    http = require('http'),
+    https = require('http'),
     url = require('url'),
     fs = require('fs'),
     path = require('path'),
@@ -19,16 +21,97 @@ require.paths.unshift(path.join(config.app_dir, '../', '.node_libraries'));
 
 var daemon = require('daemon');
 
+var proxy = new httpProxy.HttpProxy();
+var proxymap = {};
+
+var load_proxymap = function (fname, cb) {
+  fs.readFile(fname, function (err, data) {
+    if (err) {
+      console.error('load_proxymap read error: %s', err.toString());
+      cb(err, undefined);
+    } else {
+      try {
+        var map = JSON.parse(data);
+        if (typeof map.router != 'null') {
+          proxymap = {};
+          for(var i in map.router) {
+            if (map.router.hasOwnProperty(i)) {
+              var prts = map.router[i].split(':');
+              proxymap[i] = {host: prts[0], port: prts[1]};
+            }
+          }
+          cb(undefined, true);
+        } else {
+          cb('no_map_defined', undefined);
+        }
+      } catch (e) {
+        console.error('load_proxymap parse error: %s', e.toString());
+        cb(e, undefined);
+      }
+    }
+  });
+};
+
+var lookup_hostport = function (hostport) {
+  if (proxymap.hasOwnProperty(hostport)) {
+    return proxymap[hostport];
+  } else if (hostport.indexOf(':') > -1) {
+    var prts = hostport.split(':');
+    if (proxymap.hasOwnProperty(prts[0])) {
+      return proxymap[prts[0]];
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
+
+var handle_http_request = function (req, res) {
+  if (typeof req.headers.host == 'string') {
+    var hostport = lookup_hostport(req.headers.host);
+    if (hostport != null) {
+      proxy.proxyRequest(req, res, hostport);
+    } else {
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('hostname not known');
+    }
+  } else {
+    res.writeHead(406, {'Content-Type': 'text/plain'});
+    res.end('no hostname in request');
+  }
+};
+
+var handle_upgrade_request = function (req, socket, head) {
+  if (typeof req.headers.host == 'string') {
+    var hostport = lookup_hostport(req.headers.host);
+    if (hostport != null) {
+      proxy.proxyWebSocketRequest(req, socket, head, hostport);
+    } else {
+      socket.end();
+      socket.destroy();
+    }
+  } else {
+    socket.end();
+    socket.destroy();
+  }
+};
+
+var switch_user = function () {
+  var child = exec('id -u ' + config.opt.userid, function (err, stdout, stderr) {
+      daemon.setreuid(parseInt(stdout));
+      console.log('Switched to ' + process.getuid() + '.');
+  });
+}
 
 lib.update_proxytable_map(function (err) {
   if (err) {
     console.log("err: " + JSON.stringify(err));
   } else {
-    var proxy = httpProxy.createServer({router: config.opt.proxy_table_file, silent: true, hostname_only: true});
-    proxy.listen(80);
-    proxy.addListener('updateRoutes', function () {
-      console.log('updateRoutes fired');
-    });
+    var http_server = http.createServer(handle_http_request);
+    http_server.on('upgrade', handle_upgrade_request);
+    http_server.listen(80);
+    console.log('Nodester started on port 80');
     if (config.opt.enable_ssl === true) {
       var https = require('https');
       var options = {
@@ -43,12 +126,10 @@ lib.update_proxytable_map(function (err) {
       httpSsl.setMaxListeners(1000);
       httpSsl.listen(443);
       console.log('Nodester started on port 443');
+      switch_user();
+    } else {
+      switch_user();
     }
-    var child = exec('id -u ' + config.opt.userid, function (err, stdout, stderr) {
-        daemon.setreuid(parseInt(stdout));
-        console.log('Switched to ' + process.getuid() + '.');
-    });
-    console.log('Nodester started on port 80');
   }
 });
 
