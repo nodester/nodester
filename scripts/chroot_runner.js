3819c9408caefd7868ed762263f7fcc310ee12e6
DanBUK <dan@f-box.org>
2011-07-26 13:32:47 +0100

    lets get that actually working shall we..

--- scripts/chroot_runner.js
+++ scripts/chroot_runner.js
@@ -23,51 +23,6 @@ var pre_shutdown = function () {
   // if (typeof error_log_fd != 'null') fs.closeSync(error_log_fd);
 };
 
-var log_lines_stdout = new Array();
-var log_lines_stderr = new Array();
-
-var log_listen = function (p, cb) {
-  var srv = net.createServer(function (conn) {
-    var logs = JSON.stringify({
-      stdout: log_lines_stdout.join('\n'),
-      stderr: log_lines_stderr.join('\n')
-    });
-    conn.write(logs);
-    conn.end();
-  });
-  srv.listen(p, cb);
-};
-
-var log_line = function (line, stdout) {
-  if (typeof this == 'string') {
-    switch (this) {
-      case 'chroot_runner':
-        log_lines_stderr.push(this + ': ' + line);
-        if (log_lines_stderr.length > 10) log_lines_stderr.shift();
-        break;
-      case 'stdout':
-        log_lines_stdout.push(line);
-        if (log_lines_stdout.length > 10) log_lines_stdout.shift();
-        break;
-      case 'stderr':
-        log_lines_stderr.push(line);
-        if (log_lines_stderr.length > 10) log_lines_stderr.shift();
-        break;
-    }
-  }
-  /*
-  if (typeof stdout != 'undefined') {
-    if (stdout == LOG_STDOUT) {
-      console.log(line);
-    } else if (stdout == LOG_STDERR) {
-      console.error(line);
-    }
-  }
-  */
-  // if (error_log_fd !== null) fs.write(error_log_fd, line + '\n');
-  
-};
-
 var env = {
     PATH: '/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin',
     NODE_ENV: 'production'
@@ -80,82 +35,105 @@ if (config.env) {
 env.app_port = parseInt(config.port, 10);
 env.app_host = config.ip;
 var args = ['/app/' + config.start];
-var log_sock_path = path.join(config.appchroot, '.nodester', 'logs.sock');
-log_listen(log_sock_path, function () {
-  try {
-    fs.chmodSync(log_sock_path, '0777');
-  } catch (e) {
-    log_line('chroot_runner', 'Failed to chmod logs.sock', LOG_STDERR);
-  }
-  log_line('chroot_runner', 'log_listen\'ing', LOG_STDERR);
-  var chroot_res = daemon.chroot(config.appchroot);
-  if (chroot_res !== true) {
-    log_line('chroot_runner', 'Failed to chroot to ' + config.apphome, LOG_STDERR);
-    pre_shutdown();
-    process.exit(1);
-  }
-  var ch_uid = daemon.setreuid(config.userid);
-  if (ch_uid !== true) {
-    log_line.call('chroot_runner', 'Failed to change user to ' + config.userid, LOG_STDERR);
-    pre_shutdown();
-    process.exit(2);
-  }
-  var child = null;
-  var child_watcher_time = null;
+var chroot_res = daemon.chroot(config.appchroot);
+if (chroot_res !== true) {
+  log_line('chroot_runner', 'Failed to chroot to ' + config.apphome, LOG_STDERR);
+  pre_shutdown();
+  process.exit(1);
+}
+var ch_uid = daemon.setreuid(config.userid);
+if (ch_uid !== true) {
+  log_line.call('chroot_runner', 'Failed to change user to ' + config.userid, LOG_STDERR);
+  pre_shutdown();
+  process.exit(2);
+}
 
-  var myPid = daemon.start();
+var child = null;
+var child_watcher_time = null;
+var log_lines = [];
+var myPid = daemon.start();
+(function () {
+
+  var log_listen = function (p, cb) {
+    var srv = net.createServer(function (conn) {
+      var logs = JSON.stringify({
+        logs: log_lines.join('\n')
+      });
+      conn.write(logs);
+      conn.end();
+    });
+    srv.listen(p, cb);
+  };
+
+  var log_line = function (line, stdout) {
+    if (typeof this == 'string') {
+      line = this + line;
+    }
+    log_lines.push(line);
+    if (log_lines.length > 150) log_lines.shift();
+  };
+
   log_line.call('chroot_runner', 'New PID: ' + myPid.toString());
   if (path.existsSync('/.nodester/pids/runner.pid')) fs.unlinkSync('/.nodester/pids/runner.pid');
   fs.writeFileSync('/.nodester/pids/runner.pid', myPid.toString());
 
-  process.on('SIGINT', function () {
-    log_line.call('chroot_runner', 'SIGINT recieved, sending SIGTERM to children.');
-    if (child !== null) {
-      log_line.call('chroot_runner', 'Child PID: ' + child.pid.toString());
-      process.kill(child.pid, 'SIGTERM');
-      process.exit();
-    } else {
-      process.exit();
+  var log_sock_path = path.join('/', '.nodester', 'logs.sock');
+  log_listen(log_sock_path, function () {
+    log_line('chroot_runner', 'log_listen\'ing', LOG_STDERR);
+    try {
+      fs.chmodSync(log_sock_path, '0777');
+    } catch (e) {
+      log_line('chroot_runner', 'Failed to chmod logs.sock', LOG_STDERR);
     }
-  });
-
-  process.on('SIGTERM', function () {
-    log_line.call('chroot_runner', 'SIGTERM recieved, sending SIGTERM to children.');
-    if (child !== null) {
-      log_line.call('chroot_runner', 'Child PID: ' + child.pid.toString());
-      process.kill(child.pid, 'SIGTERM');
-      process.exit();
-    } else {
-      process.exit();
-    }
-  });
-
-  var start_child = function () {
-    child = spawn('/usr/bin/node', args, { env: env });
-    child.stdout.on('data', log_line.bind('stdout'));
-    child.stderr.on('data', log_line.bind('stderr'));
-    child.on('exit', function (code) {
-      if (code > 0 && run_count > run_max) {
-        log_line.call('Watcher', 'Error: Restarted too many times, bailing.', LOG_STDERR);
-        clearInterval(child_watcher_timer);
-        pre_shutdown();
-        process.exit(3);
-      } else if (code > 0) {
-        log_line.call('Watcher', 'Process died with exit code ' + code + '. Restarting...', LOG_STDERR);
-        child = null;
+    process.on('SIGINT', function () {
+      log_line.call('chroot_runner', 'SIGINT recieved, sending SIGTERM to children.');
+      if (child !== null) {
+        log_line.call('chroot_runner', 'Child PID: ' + child.pid.toString());
+        process.kill(child.pid, 'SIGTERM');
+        process.exit();
       } else {
-        log_line.call('Watcher', 'Process exited cleanly. Dieing.', LOG_STDERR);
-        clearInterval(child_watcher_timer);
-        pre_shutdown();
-        process.exit(0);
+        process.exit();
       }
     });
-  };
-  var child_watcher = function () {
-    if (child === null) {
-      start_child();
-      run_count++;
-    }
-  };
-  child_watcher_timer = setInterval(child_watcher, 750);
-});
+
+    process.on('SIGTERM', function () {
+      log_line.call('chroot_runner', 'SIGTERM recieved, sending SIGTERM to children.');
+      if (child !== null) {
+        log_line.call('chroot_runner', 'Child PID: ' + child.pid.toString());
+        process.kill(child.pid, 'SIGTERM');
+        process.exit();
+      } else {
+        process.exit();
+      }
+    });
+
+    var start_child = function () {
+      child = spawn('/usr/bin/node', args, { env: env });
+      child.stdout.on('data', log_line.bind('stdout'));
+      child.stderr.on('data', log_line.bind('stderr'));
+      child.on('exit', function (code) {
+        if (code > 0 && run_count > run_max) {
+          log_line.call('Watcher', 'Error: Restarted too many times, bailing.', LOG_STDERR);
+          clearInterval(child_watcher_timer);
+          pre_shutdown();
+          process.exit(3);
+        } else if (code > 0) {
+          log_line.call('Watcher', 'Process died with exit code ' + code + '. Restarting...', LOG_STDERR);
+          child = null;
+        } else {
+          log_line.call('Watcher', 'Process exited cleanly. Dieing.', LOG_STDERR);
+          clearInterval(child_watcher_timer);
+          pre_shutdown();
+          process.exit(0);
+        }
+      });
+    };
+    var child_watcher = function () {
+      if (child === null) {
+        start_child();
+        run_count++;
+      }
+    };
+    child_watcher_timer = setInterval(child_watcher, 750);
+  });
+})();
