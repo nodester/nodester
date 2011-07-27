42ecf43ac9fd44f7c1bf129eae12d4c5c07a020d
DanBUK <dan@f-box.org>
2011-07-27 10:16:25 +0100

    Fixes nodester-cli/issues/22

--- scripts/removeapp.js
+++ scripts/removeapp.js
@@ -3,18 +3,21 @@
 var exec = require('child_process').exec;
 
 var app_dir = process.argv[2];
-var git_dir = app_dir + '.git';
+var git_dir = process.argv[3];
 
 var cmds = [
-  'rm -Rf ' + app_dir,
-  'rm -Rf ' + git_dir
+  'rm -Rf ' + app_dir + '*',
+  'rm -Rf ' + git_dir + '*'
 ];
 
 var do_cmd = function () {
   if (cmds.length > 0) {
     var cmd = cmds.shift();
+    console.log(cmd);
     exec(cmd, function () {
       do_cmd();
     });
   }
 };
+
+do_cmd();
