bbf5ca72df3e926611f337bab70fd14fa5391ab8
DanBUK <dan@f-box.org>
2011-08-03 09:52:47 +0100

    Lets fix user folder creation.

--- bin/create_user_dir.js
+++ bin/create_user_dir.js
@@ -3,18 +3,22 @@
 var exec = require('child_process').exec;
 var config = require('../config.js').opt;
 
-var cmd = 'mkdir ' + process.argv[2];
+var dirs_string = config.git_home_dir + '/' + process.argv[2] + ' ' + config.apps_home_dir + '/' + process.argv[2];
 
 var cmds = [
-  'mkdir ' + process.argv[2],
-  'chown ' + config.userid + ':' + config.userid + ' ' + process.argv[2]
+  'mkdir ' + dirs_string,
+  'chown ' + config.git_user + ':' + config.app_uid + ' ' + dirs_string,
+  'chmod 0775 ' + dirs_string
+  
 ];
 
 var do_cmd = function () {
   if (cmds.length > 0) {
     var cmd = cmds.shift();
+    console.log('Running: ' + cmd);
     exec(cmd, function () {
       do_cmd();
     });
   }
 };
+do_cmd();
