56a4f48291e1dcf1114e7fc595f022c7fa7aaa26
Contra <contra@maricopa.edu>
2011-09-14 16:38:19 -0700

    formatting

--- scripts/gitreset.js
+++ scripts/gitreset.js
@@ -5,16 +5,13 @@ var exec = require('child_process').exec;
 var app_dir = process.argv[2];
 var git_dir = app_dir + '.git';
 
-var cmds = [
-  'rm -Rf ' + app_dir,
-  'git clone ' + git_dir + ' ' + app_dir
-];
+var cmds = ['rm -Rf ' + app_dir, 'git clone ' + git_dir + ' ' + app_dir];
 
 var do_cmd = function () {
-  if (cmds.length > 0) {
-    var cmd = cmds.shift();
-    exec(cmd, function () {
-      do_cmd();
-    });
-  }
-};
+    if (cmds.length > 0) {
+      var cmd = cmds.shift();
+      exec(cmd, function () {
+        do_cmd();
+      });
+    }
+  };
