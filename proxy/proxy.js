18c94011348ac39c3c4cd481bb439e7f0f63a12b
Aaron Murray <aaron.thomas.murray@gmail.com>
2011-09-16 04:24:32 -0700

    Edited proxy/proxy.js via GitHub

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -27,7 +27,7 @@ var proxymap = {};
 var proxy_refresh_timer = null;
 
 // Ghetto hack
-var errorPage = '<html><head><title id="title">{title}</title></head><body><br/><br/><br/><br/><center><img src="http://nodester.com/images/rocket-md-right.png" alt="logo" /><h1 style ="color:#000;font-family:Arial,Helvetica,sans-serif;font-size:30px;font-weight:bold;letter-spacing:-2px;padding:0 0 5px;margin:0;">{code}</h1><h3 style ="color:#000;font-family:Arial,Helvetica,sans-serif;font-size:20px;font-weight:bold;letter-spacing:-2px;padding:0 0 5px;margin:0;">{error}</h3></center></body></html>';
+var errorPage = '<html><head><title id="title">{title}</title></head><body><br/><br/><br/><br/><center><img src="http://nodester.com/images/rocket-md-right.png" alt="logo" /><h1 style ="color:#000;font-family:Arial,Helvetica,sans-serif;font-size:38px;font-weight:bold;letter-spacing:-2px;padding:0 0 5px;margin:0;">{code}</h1><h3 style ="color:#000;font-family:Arial,Helvetica,sans-serif;font-size:24px;font-weight:bold;padding:0 0 5px;margin:0;">{error}</h3></center></body></html>';
 
 var getErrorPage = function(title, code, error) {
     return errorPage.replace('{title}', title).replace('{code}', code).replace('{error}', error);
