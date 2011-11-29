35502d32262025a2610b57cdc04e7da737d4a78c
Contra <contra@maricopa.edu>
2011-11-29 16:16:29 -0700

    proxy rewrite for bouncy

--- proxy/proxy.js
+++ proxy/proxy.js
@@ -1,188 +1,58 @@
 #!/usr/bin/env node
 
-/*
-
-Nodester - Nodejs hosting
-This app runs on port 80 and forwards traffic to the appropriate node app 
-
-*/
-
-var httpProxy = require('../deps/node-http-proxy/node-http-proxy.js'),
-    http = require('http'),
-    https = require('http'),
-    net = require('net'),
-    policyfile = require('policyfile'),
-    url = require('url'),
+var lib = require('../lib/lib'),
     fs = require('fs'),
-    path = require('path'),
-    lib = require('../lib/lib'),
-    exec = require('child_process').exec,
-    config = require('../config');
+    bouncy = require('bouncy');
 
-require.paths.unshift(path.join(config.app_dir, '../', '.node_libraries'));
-
-var daemon = require('daemon');
-var proxy = new httpProxy.HttpProxy();
 var proxymap = {};
-var proxy_refresh_timer = null;
 
 // Ghetto hack
 var errorPage = '<html><head><title id="title">{title}</title></head><body><br/><br/><br/><br/><br/><center><img src="http://nodester.com/images/rocket-md-right.png" alt="logo" /><br/><h1 style ="color:#000;font-family:Arial,Helvetica,sans-serif;font-size:38px;font-weight:bold;letter-spacing:-2px;padding:0 0 5px;margin:0;">{code}</h1><h3 style ="color:#000;font-family:Arial,Helvetica,sans-serif;font-size:24px;font-weight:bold;padding:0 0 5px;margin:0;">{error}</h3></center></body></html>';
+var getErrorPage = function (title, code, error) {
+        return errorPage.replace('{title}', title).replace('{code}', code).replace('{error}', error);
+    };
 
-var getErrorPage = function(title, code, error) {
-    return errorPage.replace('{title}', title).replace('{code}', code).replace('{error}', error);
-};
-
-var queue_proxy_map_refresh = function() {
-  if (proxy_refresh_timer === null) {
-    proxy_refresh_timer = setTimeout(function() {
-      load_proxymap(config.opt.proxy_table_file, function(err, res) {
-        if (err) {
-          console.error('failed to load proxymap: %s', err.toString());
-        }
-        proxy_refresh_timer = null;
-      });
-    }, 5 * 1000);
-  }
-};
-
-fs.watchFile(config.opt.proxy_table_file, function(oldts, newts) {
-  queue_proxy_map_refresh();
+//Update proxymap any time the routing file is updated
+fs.watchFile(config.opt.proxy_table_file, function (oldts, newts) {
+    proxymap = JSON.parse(newts);
 });
 
-var load_proxymap = function(fname, cb) {
-  fs.readFile(fname, function(err, data) {
+//Don't crash br0
+process.on('uncaughtException', function (err) {
+    console.log(err.stack);
+});
+
+// Pulls out DB records and puts them in a routing file
+lib.update_proxytable_map(function (err) {
     if (err) {
-      console.error('load_proxymap read error: %s', err.toString());
-      cb(err, undefined);
+        console.log("err writing proxy file: " + JSON.stringify(err));
     } else {
-      try {
-        var map = JSON.parse(data);
-        if (typeof map.router != 'null') {
-          proxymap = {};
-          for (var i in map.router) {
-            if (map.router.hasOwnProperty(i)) {
-              var prts = map.router[i].split(':');
-              proxymap[i] = {
-                host: prts[0],
-                port: prts[1]
-              };
-            }
-          }
-          cb(undefined, true);
+        console.log('Proxy file written successfully!');
+    }
+});
+
+bouncy(function (req, bounce) {
+    var host = (req.headers.host || '').replace(/:\d+$/, '');
+    var route = proxymap[host] || proxymap[''];
+
+    if (Array.isArray(route)) {
+        // jump to a random route on arrays
+        route = route[Math.floor(Math.random() * route.length)];
+    }
+
+    if (typeof route === 'string') {
+        var s = route.split(':');
+        if (s[1]) {
+            bounce(s[0], s[1]);
         } else {
-          cb('no_map_defined', undefined);
+            bounce(s);
         }
-      } catch (e) {
-        console.error('load_proxymap parse error: %s', e.toString());
-        cb(e, undefined);
-      }
-    }
-  });
-};
-
-var lookup_hostport = function(hostport) {
-  if (proxymap.hasOwnProperty(hostport)) {
-    return proxymap[hostport];
-  } else if (hostport.indexOf(':') > -1) {
-    var prts = hostport.split(':');
-    if (proxymap.hasOwnProperty(prts[0])) {
-      return proxymap[prts[0]];
+    } else if (route) {
+        bounce(route);
     } else {
-      return null;
+        var res = bounce.respond();
+        res.statusCode = 404;
+        res.write(getErrorPage('404 - Application not found!', '404', 'Application does not exist'));
+        res.end();
     }
-  } else {
-    return null;
-  }
-};
-
-var handle_http_request = function(req, res) {
-  if (typeof req.headers.host == 'string') {
-    if (req.headers.host == 'nodefu.com' || req.headers.host == 'www.nodefu.com') {
-      res.writeHead(301, {Location: 'http://nodester.com'});
-      res.end();
-    } else {
-      var options = lookup_hostport(req.headers.host);
-      if (options !== null) {
-        req.headers['x-forwarded-for'] = req.connection.remoteAddress || req.connection.socket.remoteAddress;
-        req.headers['x-forwarded-port'] = req.connection.remotePort || req.connection.socket.remotePort;
-        req.headers['x-forwarded-proto'] = res.connection.pair ? 'https' : 'http';
-        options['allow_xforwarded_headers'] = false;
-        proxy.proxyRequest(req, res, options);
-      } else {
-        res.writeHead(404, {
-          'Content-Type': 'text/html'
-        });
-        res.end(getErrorPage('Page not found', 404, 'Application does not exist!'));
-      }
-    }
-  } else {
-    res.writeHead(406, {
-      'Content-Type': 'text/html'
-    });
-    res.end(getErrorPage('Page not found', 406, 'You didn\'t specify a hostname!'));
-  }
-};
-
-var handle_upgrade_request = function(req, socket, head) {
-  if (typeof req.headers.host == 'string') {
-    var options = lookup_hostport(req.headers.host);
-    if (options !== null) {
-      req.headers['x-forwarded-for'] = req.connection.remoteAddress || req.connection.socket.remoteAddress;
-      req.headers['x-forwarded-port'] = req.connection.remotePort || req.connection.socket.remotePort;
-      proxy.proxyWebSocketRequest(req, socket, head, options);
-    } else {
-      socket.end();
-      socket.destroy();
-    }
-  } else {
-    socket.end();
-    socket.destroy();
-  }
-};
-
-
-var switch_user = function() {
-  var child = exec('id -u ' + config.opt.userid, function(err, stdout, stderr) {
-    daemon.setreuid(parseInt(stdout, 10));
-    //console.log('Switched to ' + process.getuid() + '.');
-  });
-};
-
-lib.update_proxytable_map(function(err) {
-  if (err) {
-    console.log("err: " + JSON.stringify(err));
-  } else {
-    queue_proxy_map_refresh();
-    var http_server = http.createServer(handle_http_request);
-    http_server.on('upgrade', handle_upgrade_request);
-    http_server.listen(80, '0.0.0.0');
-    console.log('Nodester started on port 80');
-    var flash_server = policyfile.createServer();
-    flash_server.listen(843);
-    console.log('Flash Policy Server started on port 843.');
-    if (config.opt.enable_ssl === true) {
-      var https = require('https');
-      var options = {
-        ca: fs.readFileSync(config.opt.app_dir + '/' + config.opt.ssl_ca_file),
-        key: fs.readFileSync(config.opt.app_dir + '/' + config.opt.ssl_key_file),
-        cert: fs.readFileSync(config.opt.app_dir + '/' + config.opt.ssl_cert_file)
-      };
-      var httpSsl = https.createServer(options, function(req, res) {
-        var proxy = new httpProxy.HttpProxy(req, res);
-        proxy.proxyRequest(4001, "127.0.0.1", req, res);
-      });
-      httpSsl.setMaxListeners(1000);
-      httpSsl.listen(443);
-      console.log('Nodester API/WWW started on port 443'); // We need SNI in node.js
-      switch_user();
-    } else {
-      switch_user();
-    }
-    console.log('Nodester started on port 80');
-  }
-});
-
-process.on('uncaughtException', function(err) {
-  console.log(err.stack);
-});
+}).listen(80);
