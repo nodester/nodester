fc9815661cd5c3c9b0972ea9a2d64aadeb8acf91
Jarrod Mosen <jarrod.mosen@me.com>
2012-03-06 17:45:18 +1300

    Started redesign

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,10 +1,10 @@
 $(function() {
 
 	$('#reqbutton').click(function() {
-		$('#reqform').toggle('slow', function() {});
+		$('#reqform').toggle(200, function() {});
 	});
 	$('#regbutton').click(function() {
-		$('#regform').toggle('slow', function() {});
+		$('#regform').toggle(200, function() {});
 	});
 
 	$('#videobutton').click(function() {
@@ -19,10 +19,10 @@ $(function() {
 			'height'		: 495,
 			'href'			: 'http://www.youtube.com/watch?v=jwsP1Ejv-_w&hd=1&feature=player_embedded#at=41'.replace(new RegExp("watch\\?v=", "i"), 'v/'),
 			'type'			: 'swf',
-			'swf'			: {
-			'wmode'			: 'transparent',
-			'allowfullscreen'	: 'true'
-		}
+			'swf'				: {
+				'wmode'						: 'transparent',
+				'allowfullscreen'	: 'true'
+			}
 		});
 	});
 
